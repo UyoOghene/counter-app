b0ca2644e1584e8a652e800dafe901d0ea8e232e
Uyooghene <uyooghene01@gmail.com>
2024-08-09 00:03:51 +0100

    good

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const likePic  = document.querySelector('#like-pic');
 const box = document.querySelector('.box');
 const likeTxt = document.querySelector('#like-Txt');
 const currentLikeSpan = document.querySelector('#like-span');
-let score = 1;
 
+let score = 0;
 const addLike = (e) =>{
-    
-  let currentScore =  score ++;
-    currentLikeSpan.textContent = currentScore;
-}
+    score += 1; 
+    currentLikeSpan.textContent = score; 
+    if (score !== 1) {
+        likeTxt.innerHTML = `<span id="like-span">${score}</span> likes`;
+    } else {
+        likeTxt.innerHTML = `<span id="like-span">${score}</span> like`;
+    }
+}    
+
 
 likePic.addEventListener('click',addLike);
 
